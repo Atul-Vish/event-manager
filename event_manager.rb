98d52605312a72bf7d1a3b15a607028fbc16b31e
Atul Vishwakarma <atulvishw240@gmail.com>
2024-10-21 07:15:29 +0530

    Implement the same logic with Ruby 'csv' library

--- event_manager.rb
+++ event_manager.rb
@@ -1,13 +1,8 @@
-puts 'Event Manager initialized'
+require 'csv'
+puts 'EventManager initialized'
 
-# We're creating a CSV parser
-# Problems with our approach : Although this works for our 'event_attendees.csv' file 
-# 1.CSV files often contain comments which are lines that start with a pound (#) character
-# 2.A column is unable to support a value which contains a comma (,) character
-lines = File.readlines('event_attendees.csv')
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
+contents = CSV.open('event_attendees.csv', headers: true)
+contents.each do |row|
+  name = row[2]
   puts name
 end
