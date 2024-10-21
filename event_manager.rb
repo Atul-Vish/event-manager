68b600c67c0b3d09c85b0892ff21e865f9089a32
Atul Vishwakarma <atulvishw240@gmail.com>
2024-10-21 14:41:23 +0530

    Display comma-separated string of legislator names

--- event_manager.rb
+++ event_manager.rb
@@ -29,9 +29,14 @@ contents.each do |row|
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
     )
     legislators = legislators.officials
+
+    legislator_names = legislators.map(&:name)
+
+    legislators_string = legislator_names.join(", ")
   rescue
     'You can find the representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 
-  puts "#{name} #{zipcode} #{legislators}"
+  
+  puts "#{name} #{zipcode} #{legislators_string}"
 end
