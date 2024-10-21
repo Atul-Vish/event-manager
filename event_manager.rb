ad8689e69fa542c0c060cbe74de8a5af7f5fd493
Atul Vishwakarma <atulvishw240@gmail.com>
2024-10-21 07:03:21 +0530

    CSV parser for event_attendees.csv file

--- event_manager.rb
+++ event_manager.rb
@@ -1,1 +1,13 @@
-puts 'Event Manger Initialized!'
+puts 'Event Manager initialized'
+
+# We're creating a CSV parser
+# Problems with our approach : Although this works for our 'event_attendees.csv' file 
+# 1.CSV files often contain comments which are lines that start with a pound (#) character
+# 2.A column is unable to support a value which contains a comma (,) character
+lines = File.readlines('event_attendees.csv')
+lines.each_with_index do |line, index|
+  next if index == 0
+  columns = line.split(",")
+  name = columns[2]
+  puts name
+end
